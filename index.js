278c1b688ec0c5f0e7731e15e44ce4eecd319c7c
github username <github email address>
2020-08-05 16:57:32 -0400

    event cards

--- index.js
+++ index.js
@@ -35,18 +35,35 @@ app.addEventListener("click", (e) => {
             citiesArray.forEach((city) => {citiesContainer.innerHTML += makeCityButton(city)})
         })
     } else if (e.target.id === "city") {
-        app.innerHTML = ""
+        app.innerHTML = `<h3 style="color: warning;">Here's every event in ${e.target.innerText}:</h3>`
         let stateId = e.target.dataset.state
         let cityId = e.target.dataset.city
         let eventsPath = statesPath + `/${stateId}/cities/${cityId}/events`
-        console.log(eventsPath)
-    }
+        fetch(eventsPath)
+            .then(function(obj){
+                return obj.json()
+              })
+                  .then(function(eventsArray) {
+                      eventsArray.forEach((event) => {app.innerHTML += makeEventCard(event)})
+                  })
+                }
 });
 
 function makeCityButton(city) {
     return `
    <button class="dropdown-item" type="button" id="city" data-city="${city.id}" data-state="${city.state_id}">${city.name}</button>
    `
+}
+
+function makeEventCard(event) {
+    return `
+    <div class="card col-md-8 mx-auto" style="max-width: 18rem; color: rgba(12, 134, 149, 0.94); background-color: #e28640;">
+      <div class="card-header">${event.name}</div>
+      <div class="card-body">
+        <h5 class="card-title">Warning card title</h5>
+        <p class="card-text">${event.description}</p>
+     </div>
+   </div><br>`
 }
 
 
