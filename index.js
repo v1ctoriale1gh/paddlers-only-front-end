34a443cb3ac1e7cff8a7a7d5bf877457aef1bf94
github username <github email address>
2020-08-05 17:38:14 -0400

    refactor

--- index.js
+++ index.js
@@ -8,17 +8,33 @@ fetch(statesPath)
       })
       .then(function(statesArray) {
           statesArray.forEach((state) => {statesContainer.innerHTML += makeStateButton(state)})
-      })
+      });
 
 function makeStateButton(state) {
      return `
     <button class="dropdown-item" type="button" id="state" data-state="${state.id}">${state.name}</button>
     `
-}
+};
 
+function makeCityButton(city) {
+    return `
+   <button class="dropdown-item" type="button" id="city" data-city="${city.id}" data-state="${city.state_id}">${city.name}</button>
+   `
+};
 
-app.addEventListener("click", (e) => {
-    if (e.target.id === "state") {
+function makeEventCard(event) {
+    return `
+    <div class="card col-md-8 mx-auto" style="max-width: 18rem; color: rgba(12, 134, 149, 0.94); background-color: #e28640;">
+      <div class="card-header">${event.name}</div>
+      <div class="card-body">
+        <h5 class="card-title">Warning card title</h5>
+        <p class="card-text">${event.description}</p>
+     </div>
+   </div>
+   <br>`
+};
+
+function changeToCitiesFromStates(e) {
     let button = document.getElementById("dropdownMenu2")
     button.innerText = "Please Choose A City"
     statesContainer.removeAttribute("id", "states-container")
@@ -34,37 +50,31 @@ app.addEventListener("click", (e) => {
         .then(function(citiesArray) {
             citiesArray.forEach((city) => {citiesContainer.innerHTML += makeCityButton(city)})
         })
-    } else if (e.target.id === "city") {
-        app.innerHTML = `<h3 style="color: warning;">Here's every event in ${e.target.innerText}:</h3>`
-        let stateId = e.target.dataset.state
-        let cityId = e.target.dataset.city
-        let eventsPath = statesPath + `/${stateId}/cities/${cityId}/events`
-        fetch(eventsPath)
-            .then(function(obj){
-                return obj.json()
+};
+
+function changeToEventsFromCities(e) {
+    app.innerHTML = `<h3 style="color: warning;">Here's every event in ${e.target.innerText}:</h3>`
+    let stateId = e.target.dataset.state
+    let cityId = e.target.dataset.city
+    let eventsPath = statesPath + `/${stateId}/cities/${cityId}/events`
+    fetch(eventsPath)
+        .then(function(obj){
+            return obj.json()
+          })
+              .then(function(eventsArray) {
+                  eventsArray.forEach((event) => {app.innerHTML += makeEventCard(event)})
               })
-                  .then(function(eventsArray) {
-                      eventsArray.forEach((event) => {app.innerHTML += makeEventCard(event)})
-                  })
-                }
+};
+
+
+app.addEventListener("click", (e) => {
+    if (e.target.id === "state") {
+        changeToCitiesFromStates(e)
+    } else if (e.target.id === "city") {
+        changeToEventsFromCities(e)
+    }
 });
 
-function makeCityButton(city) {
-    return `
-   <button class="dropdown-item" type="button" id="city" data-city="${city.id}" data-state="${city.state_id}">${city.name}</button>
-   `
-}
-
-function makeEventCard(event) {
-    return `
-    <div class="card col-md-8 mx-auto" style="max-width: 18rem; color: rgba(12, 134, 149, 0.94); background-color: #e28640;">
-      <div class="card-header">${event.name}</div>
-      <div class="card-body">
-        <h5 class="card-title">Warning card title</h5>
-        <p class="card-text">${event.description}</p>
-     </div>
-   </div><br>`
-}
 
 
 //<button class="dropdown-item" type="button"> State 1</button>
