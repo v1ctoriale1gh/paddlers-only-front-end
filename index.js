93b4f6bf52102d417d2495b4d35a2250f4b79c81
github username <github email address>
2020-08-06 12:52:55 -0400

    added comments, reworked event form, moved functions to make more sense

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
+//beginning const variables
 const statesPath = "http:/localhost:3000/states"
 const app = document.getElementById("app")
 const button = document.getElementById("dropdownMenu2")
 
+//first function call to render states on landing page
+renderStates()
 
+//one event listener to rule them all
+app.addEventListener("click", (e) => {
+    if (e.target.id === "state") {
+        e.preventDefault()
+        changeToCitiesFromStates(e)
+    } else if (e.target.id === "city") {
+        changeToEventsFromCities(e)
+    } else if (e.target.id === "newEvent") {
+        renderNewEventForm(e)
+    } else if (e.target.id === "backToStates") {
+        e.preventDefault()
+        backToStates()
+    } else if (e.target.id === "backToCities") {
+        backToCities(e)
+    }
+});
+
+//function for original landing page to render states in dropdown
 function renderStates() {
   let statesContainer = document.getElementById("states-container")
   fetch(statesPath)
@@ -14,6 +35,7 @@ function renderStates() {
       });
 }
 
+//function to go back to states from menu
 function backToStates() {
     button.innerText = "Please Choose A State"
     let eventsDiv = document.getElementById("events-div")
@@ -26,30 +48,14 @@ function backToStates() {
     renderStates()
 }
 
+//function to make each state - is called in renderStates and backToStates
 function makeStateButton(state) {
      return `
     <button class="dropdown-item" type="button" id="state" data-state="${state.id}">${state.name}</button>
     `
 };
 
-function makeCityButton(city) {
-    return `
-   <button class="dropdown-item" type="button" id="city" data-city="${city.id}" data-state="${city.state_id}">${city.name}</button>
-   `
-};
-
-function makeEventCard(event) {
-    return `
-    <div class="card col-md-8 mx-auto" style="max-width: 18rem; color: rgba(12, 134, 149, 0.94); background-color: #e28640;">
-      <div class="card-header">${event.name}</div>
-      <div class="card-body">
-        <h5 class="card-title">Warning card title</h5>
-        <p class="card-text">${event.description}</p>
-     </div>
-   </div>
-   <br>`
-};
-
+//function that changes dropdown from states to cities
 function changeToCitiesFromStates(e) {
     button.innerText = "Please Choose A City"
     let statesContainer = document.getElementById("states-container")
@@ -68,6 +74,7 @@ function changeToCitiesFromStates(e) {
         })
 };
 
+//function that changes dropdown menu to cities
 function backToCities(e) {
     button.innerText = "Please Choose A City"
     let eventsDiv = document.getElementById("events-div")   
@@ -88,6 +95,14 @@ function backToCities(e) {
         });
 }
 
+//function to make a city button - is called in changeToCitiesFromStates and backToCities
+function makeCityButton(city) {
+    return `
+   <button class="dropdown-item" type="button" id="city" data-city="${city.id}" data-state="${city.state_id}">${city.name}</button>
+   `
+};
+
+//function that changes city dropdown to menu drop down and displays event cards
 function changeToEventsFromCities(e) {
     button.innerText = "Menu"
     let citiesContainer = document.getElementById("cities-container")
@@ -115,87 +130,135 @@ function changeToEventsFromCities(e) {
               })
 };
 
+//function to make an event - is called in changeToEventsFromCities
+function makeEventCard(event) {
+    return `
+    <div class="card col-md-8 mx-auto" style="max-width: 18rem; color: rgba(12, 134, 149, 0.94); background-color: #e28640;">
+      <div class="card-header">${event.name}</div>
+      <div class="card-body">
+        <h5 class="card-title">Warning card title</h5>
+        <p class="card-text">${event.description}</p>
+     </div>
+   </div>
+   <br>`
+};
+
+
+//renders a form for a new event
 function renderNewEventForm(e) {
     let eventsDiv = document.getElementById("events-div")
     eventsDiv.innerHTML = `
     <br><br>
-    <div class="card col-md-8 mx-auto" style="max-width: 50rem; color: rgba(12, 134, 149, 0.94); background-color: #e28640;">
-    <br>
+    <div class="card col-md-8 mx-auto justify-content-center" style="max-width: 50rem; color: rgba(12, 134, 149, 0.94); background-color: #e28640;">
     <form>
-    <div class="form-group row">
-      <label for="name" class="col-sm-2 col-form-label">Name</label>
-      <div class="col-sm-10">
-        <input type="text" class="form-control" id="name">
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label for="name">Event Name:</label>
+        <input type="text" class="form-control" id="name" placeholder="Ex: Sweetwater Creek Morning Paddle">
+      </div>
+      <div class="form-group col-md-6">
+        <label for="contact">Contact:</label>
+        <input type="text" class="form-control" id="contact" placeholder="Ex: Bob Smith - bsmith@email.com">
       </div>
     </div>
-    <div class="form-group row">
-      <label for="Description" class="col-sm-2 col-form-label">Description</label>
-      <div class="col-sm-10">
-        <input type="textarea" class="form-control" id="description">
+    <div class="form-group">
+      <label for="address">Address:</label>
+      <input type="text" class="form-control" id="address" placeholder="1234 Main St">
+    </div>
+    <div class="form-group">
+      <label for="address2">Address 2:</label>
+      <input type="text" class="form-control" id="address2" placeholder="Apartment, studio, or floor">
+    </div>
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label for="city">City</label>
+        <input type="text" class="form-control" id="city">
+      </div>
+      <div class="form-group col-md-4">
+        <label for="state">State</label>
+        <select id="state" class="form-control">
+            <option>Alabama</option>
+            <option>Alaska</option>
+            <option>Arizona</option>
+            <option>Arkansas</option></option>
+            <option>California</option>
+            <option>Colorado</option>
+            <option>Connecticut</option>
+            <option>Delaware</option>
+            <option>District of Columbia</option>
+            <option>Florida</option>
+            <option>Georgia</option>
+            <option>Hawaii</option>
+            <option>Idaho</option>
+            <option>Illinois</option>
+            <option>Indiana</option>
+            <option>Iowa</option>
+            <option>Kansas</option>
+            <option>Kentucky</option>
+            <option>Louisiana</option>
+            <option>Maine</option>
+            <option>Maryland</option>
+            <option>Massachusetts</option>
+            <option>Michigan</option>
+            <option>Minnesota</option>
+            <option>Mississippi</option>
+            <option>Missouri</option>
+            <option>Montana</option>
+            <option>Nebraska</option>
+            <option>Nevada</option>
+            <option>New Hampshire</option>
+            <option>New Jersey</option>
+            <option>New Mexico</option>
+            <option>New York</option>
+            <option>North Carolina</option>
+            <option>North Dakota</option>
+            <option>Ohio</option>
+            <option>Oklahoma</option>
+            <option>Oregon</option>
+            <option>Pennsylvania</option>
+            <option>Rhode Island</option>
+            <option>South Carolina</option>
+            <option>South Dakota</option>
+            <option>Tennessee</option>
+            <option>Texas</option>
+            <option>Utah</option>
+            <option>Vermont</option>
+            <option>Virginia</option>
+            <option>Washington</option>
+            <option>West Virginia</option>
+            <option>Wisconsin</option>
+            <option>Wyoming</option>
+        </select>
+      </div>
+      <div class="form-group col-md-2">
+        <label for="zip">Zip</label>
+        <input type="text" class="form-control" id="zip">
       </div>
     </div>
-    <fieldset class="form-group">
-      <div class="row">
-        <legend class="col-form-label col-sm-2 pt-0">Radios</legend>
-        <div class="col-sm-10">
-          <div class="form-check">
-            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
-            <label class="form-check-label" for="gridRadios1">
-              First radio
-            </label>
-          </div>
-          <div class="form-check">
-            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">
-            <label class="form-check-label" for="gridRadios2">
-              Second radio
-            </label>
-          </div>
-          <div class="form-check disabled">
-            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3" disabled>
-            <label class="form-check-label" for="gridRadios3">
-              Third disabled radio
-            </label>
-          </div>
-        </div>
-      </div>
-    </fieldset>
-    <div class="form-group row">
-      <div class="col-sm-2">Checkbox</div>
-      <div class="col-sm-10">
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" id="gridCheck1">
-          <label class="form-check-label" for="gridCheck1">
-            Example checkbox
-          </label>
-        </div>
+    <div class="form-group">
+      <label for="description">Description:</label>
+      <textarea class="form-control" id="description" rows="3" placeholder="REMEMBER TO ADD EXAMPLE DESCRIPTION HERE"></textarea>
+    </div>
+    <div class="form-group">
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" id="has_cost?">
+        <label class="form-check-label" for="gridCheck">
+          Click if this Event has a cost
+        </label>
       </div>
     </div>
-    <div class="form-group row">
-      <div class="col-sm-10">
-        <button type="submit" class="btn btn-outline-info">submit</button>
+    <div class="form-group">
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" id="shuttle_provided?">
+        <label class="form-check-label" for="gridCheck">
+          Click if this Event has a shuttle provided
+        </label>
       </div>
     </div>
-  </form>
-  </div>`
+    <button type="submit" class="btn btn-outline-info">Submit</button><br>
+  </form>`
 }
 
-renderStates()
-
-app.addEventListener("click", (e) => {
-    if (e.target.id === "state") {
-        e.preventDefault()
-        changeToCitiesFromStates(e)
-    } else if (e.target.id === "city") {
-        changeToEventsFromCities(e)
-    } else if (e.target.id === "newEvent") {
-        renderNewEventForm(e)
-    } else if (e.target.id === "backToStates") {
-        e.preventDefault()
-        backToStates()
-    } else if (e.target.id === "backToCities") {
-        backToCities(e)
-    }
-});
 
 
 
